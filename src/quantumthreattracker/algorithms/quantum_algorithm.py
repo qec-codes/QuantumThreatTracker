1fb4b4f7ef2303422097e57c25941a3947bd91e4
Walden-Killick <walden.killick@cambridgeconsultants.com>
2025-01-22 13:43:34 +0000

    update quantum algorithm base class

--- src/quantumthreattracker/algorithms/quantum_algorithm.py
+++ src/quantumthreattracker/algorithms/quantum_algorithm.py
@@ -2,12 +2,14 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import List, Union
 
-from qiskit import QuantumCircuit
+from qsharp.estimator import EstimatorParams, EstimatorResult, LogicalCounts
+from qualtran import Bloq
 
 
 @dataclass
-class QuantumAlgorithmParameters(ABC):
+class AlgorithmParams(ABC):
     """Base dataclass for quantum algorithm parameters.
 
     Parameters
@@ -15,7 +17,7 @@ class QuantumAlgorithmParameters(ABC):
     protocol: str
         Cryptographic protocol. Can be:
             - 'RSA' (Rivest-Shamir-Adleman; factoring)
-            - 'DH' (Diffie-Helllman; discrete log)
+            - 'DH' (Diffie-Hellman; discrete log)
             - 'ECDH' (Elliptic Curve Diffie-Hellman; discrete log over elliptic curves)
     key_size: int
         Cryptographic key size.
@@ -27,6 +29,17 @@ class QuantumAlgorithmParameters(ABC):
 
 class QuantumAlgorithm(ABC):
     """Base class for quantum algorithms."""
+
+    @abstractmethod
+    def __init__(self, algorithm_params: AlgorithmParams):
+        """Initialise the `QuantumAlgorithm`.
+
+        Parameters
+        ----------
+        algorithm_params : AlgorithmParams
+            Quantum algorithm parameters.
+        """
+        self._algorithm_params = algorithm_params
 
     @abstractmethod
     def success_probability(self) -> float:
@@ -45,13 +58,14 @@ class QuantumAlgorithm(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def _target_unitary(self) -> QuantumCircuit:
-        """Construct the target unitary to perform period finding on.
+    def create_subroutine(self) -> tuple[Bloq, int]:
+        """Construct the subroutine which dominates the resource cost of the algorithm.
 
         Returns
         -------
-        QuantumCircuit
-            Target unitary.
+        tuple[Bloq, int]
+            Qualtran Bloq, together with the number of sequential repetitions needed in
+            the circuit.
 
         Raises
         ------
@@ -61,14 +75,13 @@ class QuantumAlgorithm(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def create_qiskit_circuits(self) -> list[tuple[QuantumCircuit, int]]:
-        """Explicitly construct all quantum circuits used in the algorithm.
+    def get_logical_counts(self) -> LogicalCounts:
+        """Create the logical counts to pass to the Azure Quantum Resource Estimator.
 
         Returns
         -------
-        list[tuple[QuantumCircuit, int]]
-            List of quantum circuits, together with the number of repetitions required
-            for each circuit.
+        LogicalCounts
+            Logical counts derived from the Qualtran Bloqs.
 
         Raises
         ------
@@ -76,3 +89,21 @@ class QuantumAlgorithm(ABC):
             If the method has not been implemented.
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def estimate_resources(
+        self, estimator_params: Union[dict, List, EstimatorParams]
+    ) -> EstimatorResult:
+        """Estimate the physical resources required for a single circuit run.
+
+        Parameters
+        ----------
+        estimator_params : Union[dict, List, EstimatorParams]
+            Parameters for the Microsoft Azure Quantum Resource Estimator.
+
+        Returns
+        -------
+        EstimatorResult
+            Physical resource estimates.
+        """
+        return self.get_logical_counts().estimate(estimator_params)
