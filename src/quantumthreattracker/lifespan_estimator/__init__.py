ea28b12b082b55dfb49c154d842424241b9df4ac
Walden-Killick <walden.killick@cambridgeconsultants.com>
2025-03-03 11:49:57 +0000

    feat: add init.py

---
+++ src/quantumthreattracker/lifespan_estimator/__init__.py
@@ -0,0 +1,6 @@
+"""Lifespan Estimator module."""
+
+from .hardware_roadmap import HardwareRoadmap
+from .lifespan_estimator import LifespanEstimator
+
+__all__ = ["HardwareRoadmap", "LifespanEstimator"]
