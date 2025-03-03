dc464fa0e675b6c54230ea06c2b6315a1d0f6097
Walden-Killick <walden.killick@cambridgeconsultants.com>
2025-03-03 11:48:02 +0000

    feat: add init.py to algorithms folder

--- src/quantumthreattracker/algorithms/__init__.py
+++ src/quantumthreattracker/algorithms/__init__.py
@@ -1,1 +1,7 @@
 """Algorithms module."""
+
+from .baseline_shor import BaselineShor
+from .gidney_ekera_basic import GidneyEkeraBasic
+from .quantum_algorithm import CryptParams, QuantumAlgorithm
+
+__all__ = ["BaselineShor", "GidneyEkeraBasic", "CryptParams", "QuantumAlgorithm"]
